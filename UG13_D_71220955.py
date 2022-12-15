f68c1cdefdadb5986df66cf409f6807c1221ba3e
vinajosephinesuruh <117901116+vinajosephinesuruh@users.noreply.github.com>
2022-12-15 16:01:25 +0700

    Add files via upload

---
+++ UG13_D_71220955.py
@@ -0,0 +1,21 @@
+import turtle
+
+t = turtle.Turtle()
+t.screen.bgcolor("purple")
+t.left(300)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(90)
+t.forward(10)
+t.left(90)
+t.forward(85)
+t.left(240)
+t.forward(85)
+t.left(90)
+t.forward(10)
+
+t.up()
+t.goto(-100, -150)
+
+cs = turtle.Screen().exitonclick()
